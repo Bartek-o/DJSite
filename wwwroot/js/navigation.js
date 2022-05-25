bbd4e94f04a99fa9607fa5e7681793a5a9ec352c
Bartek-o <bartek.ocypa@gmail.com>
2022-05-25 18:29:43 +0200

    init

---
+++ wwwroot/js/navigation.js
@@ -0,0 +1,10 @@
+﻿function mobileMenu() {
+    $("#btn-toggle-nav").toggleClass("open");
+
+    if (document.getElementById("btn-toggle-nav").classList.contains("open")) {
+        $("#navbar-list-mobile").animate({ top: "66px" }, 1000);
+    } else {
+        $("#navbar-list-mobile").animate({ top: "-134px" }, 1000);
+    }
+    
+}
